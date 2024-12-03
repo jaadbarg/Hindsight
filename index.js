e99d787ab4dfbb937e52745070c40a4e7cc3f448
John Barguti <jaadbarg@gmail.com>
2024-12-03 18:41:09 -0500

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
-
+// index.js
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App'; // Updated path
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
